d843802e515529a46e36d2707c45fe6ca22a9afb
William <Williamjackson2734@my.whccd.edu>
2024-12-01 22:36:04 -0800

    Added my own personal thoughts and answers to the questions.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # COMP002.Assignment7
+Author: William Jackson
+Purpose: Week 7 Assignment 
---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. Write a console.log() statement that answers the following question: What worked
+// well for you over the last 9 weeks that aided in your learning process?
+
+console.log(
+  "What worked well for you over the last 9 weeks that aided in your learning process?"
+);
+console.log("Well, the only thing I ever manage to get right was Week 2 since I did got the hang of program structures and wanting to do more practice with it on how to reshape it and yet not to mention Week 4 was also a good learning on doing the Data Structures since I did found out on how to place down spacific structures that can allow me to organized the data and yet keep it cryptied since crypted data does also play a role if you dont wanna get hacked.");
+
+// 2. Write a console.log() statement that answers the following question: What would you
+// change about the course?
+
+console.log("What would you change about the course?");
+console.log("The one thing to change the course is on taking a look of the code and see whats wrong with it on why it keeps on erroring like there should be something in visual studio code that can tell you whats wrong with your code and how to fix it so the code can run the way you want it too.");
+
+// 3. Write a console.log() statement that answers the following question: Did you learn
+// what you expected to learn in this course?
+
+console.log("Did you learn what you expected to learn in this course?");
+console.log("Well I did learn on that Visual Studio code will play a good role in java and java script since I do wanna use java and javascript to make creative creations but then eversince I saw the Eloquent JavaScript 4th edition 2024 I've seen how to place down a good format on what you want your code to say and calculate and to even make a multiplication chart too.");
+
+// 4. Write a console.log() statement that answers the following question: What concepts
+// were new to you and what concepts had you encountered already in the past?
+
+console.log(
+  "What concepts were new to you and what concepts had you encountered already in the past?"
+);
+console.log("I never encountered anything in the past with it minus minecraft but thats off topic anyways the concept that I still struggle with that cost me to get a 61% in this class and high bets I will be hold back for an entire year and get out at 2027 or 2028 since I will end up being in my late 20s at that time so I might just drop out and quit if that happens but thats off topic too my most difficult concept was the week 5 assignment expection handling that is what I most struggle with no matter what I did it just wont stop erroring and dont even get me started on the week 6 assignment HTTP and Forms That keeps erroring no matter what I do I checked if it is all good I checked every flaw to fix like no matter what I did it wont work!....");
